701a637d0cac8c2869a93688c343dc4bf4f7f188
Ivan Paner <ivan@thinkingmachin.es>
2025-09-11 16:34:18 +0800

    Update server

--- src/server.py
+++ src/server.py
@@ -1,8 +1,14 @@
 from fastmcp import FastMCP
 from dataclasses import dataclass
 import random
+import whenever
+
+from fastmcp.server.middleware.rate_limiting import RateLimitingMiddleware
 
 mcp = FastMCP(name="Ivan Paner's Resume")
+mcp.add_middleware(
+    RateLimitingMiddleware(max_requests_per_second=10.0, burst_capacity=20)
+)
 
 
 @dataclass
@@ -15,11 +21,12 @@ class PersonalDetails:
     mobile_number: str
     linkedin: str
     how_to_contact: str
+    availability: str
 
 
 @mcp.tool
 def personal_details() -> PersonalDetails:
-    """Details on how to contact Ivan, his location, languages"""
+    """Details on how to contact Ivan, his location, languages, availability"""
     return PersonalDetails(
         given_name="Ivan",
         surname="Paner",
@@ -28,30 +35,64 @@ def personal_details() -> PersonalDetails:
         email="ivan.paner@gmail.com",
         mobile_number="+63 917 874 3339",
         linkedin="https://www.linkedin.com/in/ivan-paner/",
-        how_to_contact="Send an email or message via LinkedIn!",
+        how_to_contact="Please send an email or message via LinkedIn!",
+        availability="Ivan is currently employed with Thinking Machines Data Science, Inc.",
     )
+
+
+@dataclass
+class Role:
+    title: str
+    role_start_date: str
+    role_end_date: str
+    role_tenure: str
 
 
 @dataclass
 class WorkExperience:
     company: str
-    role: str
-    start_date: str
-    end_date: str
+    roles: list[Role]
+    company_start_date: str
+    company_end_date: str
+    company_tenure: str
     highlights: list[str]
+    website: str | None = None
 
 
 @mcp.tool
 def work_experience() -> list[WorkExperience]:
     """Get Ivan Paner's work experience"""
+    current_date = whenever.Date.today_in_system_tz()
+    tm_mle_start_date = whenever.Date(2023, 4, 1)
+    tm_mle_tenure = current_date - tm_mle_start_date
+    tm_mle_years = f"{tm_mle_tenure.in_years_months_days()[0]} years"
+
+    tm_start_date = whenever.Date(2021, 4, 1)
+    tm_tenure = current_date - tm_start_date
+    tm_years = f"{tm_tenure.in_years_months_days()[0]} years"
     return [
         WorkExperience(
             company="Thinking Machines Data Science, Inc.",
-            role="Machine Learning Engineer",
-            start_date="April 2021",
-            end_date="Present",
+            website="https://thinkingmachin.es/",
+            roles=[
+                Role(
+                    title="Mid Machine Learning Engineer",
+                    role_start_date="April 2021",
+                    role_end_date="April 2023",
+                    role_tenure="2 years",
+                ),
+                Role(
+                    title="Senior Machine Learning Engineer",
+                    role_start_date="April 2023",
+                    role_end_date="Present",
+                    role_tenure=tm_mle_years,
+                ),
+            ],
+            company_start_date="April 2021",
+            company_end_date="Present",
+            company_tenure=tm_years,
             highlights=[
-                "Led technical architecture and implementation of Azure OpenAI LLM-powered systems at an ASEAN state-owned investment firm, including an employee performance evaluation system, automated due diligence report generation, and an enterprise knowledge base.",
+                "Led technical architecture and implementation of Azure OpenAI LLM-powered systems at an ASEAN state-owned investment firm, including an employee performance evaluation system, automated due diligence report generation, and a RAG enterprise knowledge base.",
                 "Led the migration and deployment of 44 ML production models from Cloudera to AWS within eight months for a major SEA telecommunications company. I served as both technical lead and project manager, continuously aligning deliverables with stakeholders from the company and AWS. My technical contributions include setting up backend components to allow for seamless communication and data flow between Airflow, EMR, SageMaker, and Snowflake.",
                 "Developed linear regression models for a major SEA telecommunications company as part of a POC to migrate their data platform to GCP. After finishing two weeks ahead of schedule, I continued development and upsold the company on ML services within GCP that are outside the project’s scope.",
                 "Developed, deployed, and maintained pipelines for ingesting, transforming, and storing datasets from fourteen data providers for an ASEAN state-owned investment firm. The datasets include information on e-commerce, company financial statements, transportation and logistics, and employee profiles.",
@@ -62,9 +103,23 @@ def work_experience() -> list[WorkExperience]:
         ),
         WorkExperience(
             company="Augmented Intelligence Pros, Inc.",
-            role="AI Software Developer",
-            start_date="April 2017",
-            end_date="April 2021",
+            roles=[
+                Role(
+                    title="AI Software Developer",
+                    role_start_date="April 2017",
+                    role_end_date="September 2019",
+                    role_tenure="2 years",
+                ),
+                Role(
+                    title="NLP Tech Lead",
+                    role_start_date="September 2019",
+                    role_end_date="April 2021",
+                    role_tenure="2 years",
+                ),
+            ],
+            company_start_date="April 2017",
+            company_end_date="April 2021",
+            company_tenure="4 years",
             highlights=[
                 "Contributed to microservice systems-level design of an in-house conversational agent for the BPO sector which allowed teams to quickly deliver features independent of each other.",
                 "Assisted in writing patents about the text classification, data flow, and data structure technologies behind the in-house conversational agent.",
@@ -78,9 +133,17 @@ def work_experience() -> list[WorkExperience]:
         ),
         WorkExperience(
             company="Dimension Data Asia Pacific",
-            role="Junior Software Dveloper",
-            start_date="May 2015",
-            end_date="Aug 2016",
+            roles=[
+                Role(
+                    title="Junior Software Developer",
+                    role_start_date="May 2015",
+                    role_end_date="Aug 2016",
+                    role_tenure="1 year",
+                ),
+            ],
+            company_start_date="May 2015",
+            company_end_date="Aug 2016",
+            company_tenure="1 year",
             highlights=[
                 "Performed technical research, software development, and testing using Xamarin.",
                 "Researched emerging technologies and industry practices including big data and DevOps to improve pre-sales operations.",
@@ -139,7 +202,7 @@ class EducationDetail:
     country: str
     degree: str
     graduation_year: str
-    awards: list[str]
+    notes: list[str]
 
 
 @mcp.tool
@@ -151,7 +214,7 @@ def education_details() -> list[EducationDetail]:
             country="Philippines",
             degree="B.S. in Computer Science, Specialization in Software Technology",
             graduation_year="2015",
-            awards=["Graduated Cum Laude", "Most Outstanding Thesis Award"],
+            notes=["Graduated Cum Laude", "Most Outstanding Thesis Award"],
         ),
     ]
 
@@ -203,7 +266,7 @@ def server_meta_details() -> list[str]:
     return [
         "Details were last updated on September 5, 2025",
         "MCP Server was built using FastMCP (https://github.com/jlowin/fastmcp)",
-        "MCP Server is deployed using Cloudflare Workers",
+        "MCP Server is deployed in Railway",
         "Resume details are implemented as MCP tools instead of resources, even if they are read-only. This is because Claude Desktop doesn't pull resources automatically: users have to attach them manually. See https://github.com/modelcontextprotocol/python-sdk/issues/1016 for the discussion.",
     ]
 
